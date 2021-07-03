681025d43c954b5829c37928248cb0dda1ec8d0a
ivanjbmak <ivanjbmak@gmail.com>
2021-07-03 01:37:50 -0400

    adding navbar headings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,32 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
     <div>
-      <Box bg="gray.800" w="100vw" p={4} color="purple.200">
-        Ivan Mak
+      <Box
+        className="container"
+        bg="gray.800"
+        w="100vw"
+        p={4}
+        color="purple.200"
+      >
+        Ivan Mak{" "}
+        <Button className="button" colorScheme="purple" variant="ghost">
+          Projects
+        </Button>
+        <Button colorScheme="purple" variant="ghost">
+          Experience
+        </Button>
+        <Button colorScheme="purple" variant="ghost">
+          Education
+        </Button>
+        <Button colorScheme="purple" variant="ghost">
+          Contact & Resume
+        </Button>
+        <Button colorScheme="purple" variant="ghost">
+          Dark Mode Button
+        </Button>
       </Box>
     </div>
   );
