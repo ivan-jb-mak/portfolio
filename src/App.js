94b6444d860fea31274d692d67f2ec069753ae24
ivan-jb-mak <ivanjbmak@gmail.com>
2021-07-03 18:45:26 -0400

    adding initial project card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Project from "./components/Project";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Project />
     </div>
   );
 };
---
+++ src/components/Project.js
@@ -0,0 +1,13 @@
+import { Box, Image } from "@chakra-ui/react";
+import React from "react";
+import devbook from "../assets/Devbook.jpg";
+
+const Project = () => {
+  return (
+    <Box>
+      <Image src="devbook"></Image>
+    </Box>
+  );
+};
+
+export default Project;
